d2ec2b125dda3083745b58d391cd98c65c4b7d85
PrasadKnd <126407158+PrasadKnd@users.noreply.github.com>
2023-05-15 03:43:38 +0530

    Add files via upload

---
+++ Utility.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleSpecflowProject.Utilities
+{
+    public static class Utility
+    {
+
+        public static void ScrollTopOfThePage(IWebDriver driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0,0);");
+        }
+    }
+}
